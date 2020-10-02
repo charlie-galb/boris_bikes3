ef2526384f5b13f8a2a04077d166e5bc832d1e3a
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-02 15:58:37 +0100

    Created private full/empty methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,12 +7,24 @@ class DockingStation
   end
 
   def release_bike
-    return raise "Docking station empty" if @stock.empty?
+    return raise "Docking station empty" if empty?
     @stock.pop
   end
   def dock(bike)
-    return raise "Bike station full" if @stock.length == 20
-
+    return raise "Bike station full" if full?
     @stock << bike
   end
+
+  private
+  def full?
+    @stock.length >= 20 ? true : false
+    #return raise "Bike station full" if @stock.length >= 20
+  end
+
+  def empty?
+    @stock.empty?
+  end
+
+
+
 end
